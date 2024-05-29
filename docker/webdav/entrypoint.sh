9f7089aa4d71923da821e131eb1c6b50d971f07d
Raphael <baumgartner@mailbox.org>
2024-05-29 06:51:36 +0200

    Add media services and remove unwanted services

--- docker/webdav/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-htpasswd -bc /etc/nginx/htpasswd $USERNAME $PASSWORD
-mediaowner=$(ls -ld /media | awk '{print $3}')
-echo "Current /media owner is $mediaowner"
-if [ "$mediaowner" != "www-data" ]
-then
-    chown -R www-data:www-data /media
-fi
